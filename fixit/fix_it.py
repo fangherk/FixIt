79dcd9c7ef04739f2bcf247edaf317f4b383b211
Herrick Fang <herrickfang@gmail.com>
2019-02-22 15:07:14 -0800

    Add input handling in the main game and some cleanup (#14)

--- fixit/fix_it.py
+++ fixit/fix_it.py
@@ -1,9 +1,9 @@
 import random
 from collections import deque
 
-from fixit.Order import Order
-from fixit.MatchingEngine import MatchingEngine
 from fixit.AccountingBook import AccountingBook
+from fixit.MatchingEngine import MatchingEngine
+from fixit.Order import Order
 from fixit.Player import Player
 
 
@@ -40,6 +40,25 @@ class FixItGame:
         for _, val in self.accounting.players.items():
             print(val)
 
+    def delete_order(self, person):
+        """ Delete an order """
+        try:
+            type = int(input("Press 1 for bid. Press 2 for offer\n"))
+            price = int(input("What was the price of the order?\n"))
+
+        except:
+            raise ValueError("Wrong Type or Price input")
+        else:
+            if type == 1:
+                self.engine.delete_order(person, int(price), "BUY")
+            else:
+                self.engine.delete_order(person, int(price), "SELL")
+
+    def add_order(self, person, order, order_type):
+        """ Delete an order """
+        self.engine.add_order(order, order_type)
+        self.accounting.players[person].add_order(order, order_type)
+
     def play(self):
         """Game loop"""
         num_cards = len(self.accounting.players) + self.rounds
@@ -48,42 +67,44 @@ class FixItGame:
             print("Revealing Cards: {}\n".format(self.middle[:turn]))
             while True:
 
-                buyer = input(
-                    "Press 0 to end turn. Press 1 for buy. Press 2 for sell. Press 3 for delete. \n"
-                )
-                if buyer == "0":
-                    break
-
-                person = input("What is your id?\n")
-
-                # ASSUME integer values
-                if buyer == "3":
-                    type = input("Press 1 for bid. Press 2 for offer\n")
-                    price = input("What was the price of the order?\n")
-                    if type == "1":
-                        self.engine.delete_order(person, int(price), "BUY")
-                    else:
-                        self.engine.delete_order(person, int(price), "SELL")
+                try:
+                    buyer = int(
+                        input(
+                            "\nPress:\n0 - end turn\n1 - buy\n2 - sell\n3 - delete order\n"
+                        ))
+                except:
+                    print("Please input an integer value")
                     continue
 
-                price = input("What is the price of the share?\n")
+                if not buyer:
+                    break
+                elif buyer in (1, 2):
+                    try:
+                        person = int(input("What is your id?\n"))
+                        price = int(input("What is the price of the share?\n"))
+                    except:
+                        print("Try Again.")
+                        continue
+                    else:
+                        order = Order(person, price, 1)
+                        if buyer == 1:
+                            self.add_order(person, order, "BUY")
+                        if buyer == 2:
+                            self.add_order(person, order, "SELL")
+                elif buyer == 3:
+                    self.delete_order(person)
+                else:
+                    raise ValueError("Invalid Integer")
 
-                order = Order(person, int(price), 1)
-
-                if buyer == "1":
-                    self.engine.add_order(order, "BUY")
-                    self.accounting.players[person].add_order(order, "BUY")
-                if buyer == "2":
-                    self.engine.add_order(order, "SELL")
-                    self.accounting.players[person].add_order(order, "SELL")
-
-                #Trade = namedtuple("Trade", "seller buyer score")
                 trade = self.engine.balance_orders()
 
-                print(self.engine)
                 if trade:
                     self.accounting.balance_player_orders(trade)
-                    print(self.accounting)
+
+                print("\n ----- \n")
+                print(self.engine)
+                print(self.accounting)
+                print("\n ----- \n")
 
             self.stats()
 
@@ -96,9 +117,13 @@ class FixItGame:
 def simulate_game():
     """Function to start the game"""
     game = FixItGame()
-    num_players = input("How many players are there?\t")
-    players = [str(x) for x in list(range(1, int(num_players) + 1))]
-    game.set_up_game(players)
+    try:
+        num_players = int(input("How many players are there?\t"))
+    except:
+        raise ValueError("Invalid number of players")
+    else:
+        players = [x for x in list(range(1, int(num_players) + 1))]
+        game.set_up_game(players)
 
     start = input("Start game? (y) \t")
     if start == "y":
